363a9685350c0aed4990f3f32c17e874a22adb6d
AtharvMittal138 <knjmittal@gmail.com>
2025-12-12 21:54:33 +0530

    last recursion

---
+++ Recurssion/PrintZigZag.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void PreInPost(int n){
+    if(n==0)
+    return;
+    printf("%d ",n);
+    PreInPost(n-1);
+    printf("%d ",n);
+    PreInPost(n-1);
+    printf("%d ",n);
+    return;
+}
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    PreInPost(n);
+    return 0;
+}
