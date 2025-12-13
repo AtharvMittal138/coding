dde30ba13f4bd36ddb8871d34f7a64efd44c7ffa
AtharvMittal138 <knjmittal@gmail.com>
2025-12-13 19:52:07 +0530

    newwwwwww.

---
+++ Rotate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the row and coloumn of the matrix: ");
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    printf("Enter the elements of the array\n");
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        scanf("%d",)
+    }
+}
